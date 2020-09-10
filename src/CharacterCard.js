ce5c1b394929664b03c0206786a8c083e940ffcf
ngamdarunee <s6135512038@phuket.psu.ac.th>
2020-09-10 10:39:51 +0700

    first commit

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 function CharacterCard(props) {
     const [active, setActive] = useState(false);
     const activate = () => {
+        if(!active){
         setActive(true)
-    }
+        props.activationHandler(props.value)
+        }
+        }
+       
     const className = `card ${active ? 'activeCard' : ''}`
     return (
         <div className={className} onClick={activate}>{props.value}</div>
