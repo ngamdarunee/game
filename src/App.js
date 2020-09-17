7280e88108ac94ef31cba16bea6c0ba3577ebd78
ngamdarunee <s6135512038@phuket.psu.ac.th>
2020-09-17 21:46:55 +0700

    เพิ่มข้อความต้อนรับ

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Welcome To Card Game</p>
         <WordCard value={word} />
       </header>
     </div>
